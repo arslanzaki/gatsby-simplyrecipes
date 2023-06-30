006b8d6ccfe7419cb0fae727c86f3933bc37d2de
arslanzaki <arslanzaki223@gmail.com>
2023-07-01 00:17:43 +0500

    useStaticQuery from CodeExporter

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,17 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "@arslanzaki",
+    person: { name: "john", age: 32 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "john", age: 32 },
+      { name: "kim", age: 43 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
