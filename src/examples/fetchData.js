c765dbe523c9e121b36edeb5ce8faff09c168670
arslanzaki <arslanzaki223@gmail.com>
2023-07-01 20:12:13 +0500

    useStaticQuery From Scratch

--- src/examples/fetchData.js
+++ src/examples/fetchData.js
@@ -1,32 +1,34 @@
 import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
-const ComponentName = () => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          title
-          description
-          author
-          person {
-            name
-            age
-          }
-          complexData {
-            age
-            name
-          }
-          simpleData
+const getData = graphql`
+  query {
+    site {
+      info:siteMetadata {
+        title
+        description
+        author
+        person {
+          name
+          age
         }
+        complexData {
+          age
+          name
+        }
+        simpleData
       }
     }
-  `);
+  }
+`;
+const ComponentName = () => {
+  const data = useStaticQuery(getData);
+  console.log(data);
   return (
     <div>
-      <h2>{data.site.siteMetadata.person.name}</h2>
+      <h2>{data.site.info.person.name}</h2>
       <div>
-        {data.site.siteMetadata.complexData.map((item, index) => {
+        {data.site.info.complexData.map((item, index) => {
           return (
             <p key={index}>
               {item.name}:{item.age}
